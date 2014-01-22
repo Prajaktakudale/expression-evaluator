caecf7efa7efac1c0b91df15181913f29776779d
prajakta <prajaktv@thoghtworks.com>
2014-01-22 18:42:59 +0530

    release 0.1 with test cases

--- Test/main/evaluatorTest.java
+++ Test/main/evaluatorTest.java
@@ -8,7 +8,7 @@ public class evaluatorTest {
     @Test
     public void testWillGives10IfExpression5plus5() throws Exception {
         String actual;
-        String expected = "10";
+        String expected = "10.0";
 
         Evaluator evaluate = new Evaluator();
         actual = evaluate.expressionEvaluator("5 + 5");
@@ -18,10 +18,53 @@ public class evaluatorTest {
     @Test
     public void testWillGives20IfExpression10plus10() throws Exception {
         String actual;
-        String expected = "20";
+        String expected = "20.0";
 
         Evaluator evaluate = new Evaluator();
         actual = evaluate.expressionEvaluator("10 + 10");
+
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void testWillGives0IfExpression10minus10() throws Exception {
+        String actual;
+        String expected = "0.0";
+
+        Evaluator evaluate = new Evaluator();
+        actual = evaluate.expressionEvaluator("10 - 10");
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWillGives20IfExpression5into4() throws Exception {
+        String actual;
+        String expected = "20.0";
+
+        Evaluator evaluate = new Evaluator();
+        actual = evaluate.expressionEvaluator("5 * 4");
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWillGives3IfExpression15divided5() throws Exception {
+        String actual;
+        String expected = "3.0";
+
+        Evaluator evaluate = new Evaluator();
+        actual = evaluate.expressionEvaluator("15 / 5");
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testWillGives8IfExpression2topow3() throws Exception {
+        String actual;
+        String expected = "8.0";
+
+        Evaluator evaluate = new Evaluator();
+        actual = evaluate.expressionEvaluator("2 ^ 3");
 
         Assert.assertEquals(actual, expected);
     }
--- src/sourceLib/Evaluator.java
+++ src/sourceLib/Evaluator.java
@@ -3,16 +3,23 @@ package sourceLib;
 public class Evaluator {
 
     public String expressionEvaluator(String exp) {
-        int result = 0;
+        double result = 0;
         String[] expression;
         String no1, no2, operator;
         expression = exp.split(" ");
         no1 = expression[0];
-        no2 = expression[2];
         operator = expression[1];
+        no2 = expression[2];
         if (0 == "+".compareTo(operator))
             result = Integer.parseInt(no1) + Integer.parseInt(no2);
+        if (0 == "-".compareTo(operator))
+            result = Integer.parseInt(no1) - Integer.parseInt(no2);
+        if (0 == "*".compareTo(operator))
+            result = Integer.parseInt(no1) * Integer.parseInt(no2);
+        if (0 == "/".compareTo(operator))
+            result = Integer.parseInt(no1) / Integer.parseInt(no2);
+        if (0 == "^".compareTo(operator))
+            result =  Math.pow(Integer.parseInt(no1),Integer.parseInt(no2));
         return String.valueOf(result);
     }
 }
-
